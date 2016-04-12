f29c89c42140edf5aa745e355387acf650e35037
Vladimir Urbanovich <v_urbanovich@mail.ru>
2016-04-12 21:26:27 +0300

    Add data factory

--- js/app/index.js
+++ js/app/index.js
@@ -3,4 +3,8 @@
 const calendar = angular.module('calendar', []);
 
 calendar
+    .factory('dataGenerator', require('./services/data-generator-service'));
+
+
+calendar
     .component('mainView', require('./main-view-component/main-view'));
---
+++ js/app/services/data-generator-service.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = function() {
+    return function() {
+        let dataArray = [],
+            dateNow = new Date();
+
+        //добавить числа предыдущего мес€ца
+        let firstDay = new Date(dateNow.getFullYear(), dateNow.getMonth(), 1),
+            previousMonthLastDay = new Date(firstDay - 1).getDate(),
+            numberOfPastDays = (firstDay.getDay() || 7) - 1;
+
+        for (let i = 0; i < numberOfPastDays; i += 1) {
+            let dayData = {
+                date: previousMonthLastDay - i,
+                day: numberOfPastDays - i,
+                year: new Date(firstDay - 1).getFullYear(),
+                today: false,
+                month: new Date(firstDay - 1).getMonth(),
+                events: []
+            };
+
+            dataArray.unshift(dayData);
+        }
+
+        //добавить числа мес€ца
+        let numberOfDays = new Date(dateNow.getFullYear(), dateNow.getMonth() + 1, 0).getDate();
+
+        for (let i = 0; i < numberOfDays; i += 1) {
+            let day = new Date(dateNow.getFullYear(), dateNow.getMonth(), i + 1),
+                dayData = {
+                    date: day.getDate(),
+                    day: day.getDay() || 7,
+                    today: day.getDate() === dateNow.getDate(),
+                    month: day.getMonth(),
+                    year: day.getFullYear(),
+                    events: []
+                };
+
+            dataArray.push(dayData);
+        }
+
+        //добавить дни следующего мес€ца
+        let numberOfFutureDays = 7 - dataArray.length % 7;
+
+        for (let i = 0; i < numberOfFutureDays; i += 1) {
+            let day = new Date(dateNow.getFullYear(), dateNow.getMonth() + 1, i + 1),
+                dayData = {
+                    date: day.getDate(),
+                    day: day.getDay() || 7,
+                    today: false,
+                    month: day.getMonth(),
+                    year: day.getFullYear(),
+                    events: []
+                };
+
+            dataArray.push(dayData);
+        }
+
+        return dataArray;
+    }
+};
