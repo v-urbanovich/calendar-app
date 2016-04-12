d54bd2882a2f0c235fade5ba41378ec4ad2c1c94
Vladimir Urbanovich <v_urbanovich@mail.ru>
2016-04-12 20:50:24 +0300

    Add main-view component

--- js/app/index.js
+++ js/app/index.js
@@ -1,3 +1,6 @@
 'use strict';
 
 const calendar = angular.module('calendar', []);
+
+calendar
+    .component('mainView', require('./main-view-component/main-view'));
---
+++ js/app/main-view-component/main-view.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mainViewTemplate = require('./template/main-view-template.html');
+
+const mainView = {
+    controller: function() {
+
+    },
+    template: mainViewTemplate
+};
+
+module.exports = mainView;
