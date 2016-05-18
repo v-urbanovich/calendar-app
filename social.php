37b8d1308bdd5055d9a94b7d5185b80e7324d6c9
Vladimir <v_urbanovich@mail.ru>
2016-05-18 13:50:19 +0300

    Create social.php

---
+++ social.php
@@ -0,0 +1,8 @@
+<?php
+ 
+    $title=urlencode('Заголовок вашей вкладки или веб-страницы');
+    $url=urlencode('http://v-urbanovich.github.io/calendar-app/');
+    $summary=urlencode('Текстовое описание, которое вкратце рассказывает, зачем пользователям переходить по этой ссылке.');
+    $image=urlencode('https://pbs.twimg.com/profile_images/457164810052857856/cXNMHcHF.jpeg');
+ 
+?>
